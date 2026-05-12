281fe54d018967f83266c93de156ab9bc1f3a293
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2026-05-12 11:25:26 -0400

    remove erroneous print statement from test_basics.py

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -2,7 +2,7 @@ import pytest
 from h11._util import RemoteProtocolError
 
 def test_simple_warc(verify_content_match):
-    print(verify_content_match(b"HTTP/1.1 200 OK\r\n\r\nHello, world!\r\n\r\n"))
+    verify_content_match(b"HTTP/1.1 200 OK\r\n\r\nHello, world!\r\n\r\n")
 
 def test_non_chunked_encoding(verify_content_match):
     verify_content_match(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello\r\n\r\n")
